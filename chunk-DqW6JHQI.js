a7386d20be378c8220426cb5f2e98f2530da2b0d
actionanand <actionanand@users.noreply.github.com>
2026-09-15 21:14:57 +0000

    deploy: 8e22257a86e93a4b85e583f570e928ecd3874171

--- chunk-DqW6JHQI.js
+++
@@ -1,8 +0,0 @@
-import {B as B1,_ as _1}from'./chunk-BrNAoGgV.js';import {o}from'./chunk-CNsMO7xz.js';import'./chunk-DGI8HLIo.js';import'./chunk-DwACsKG4.js';import'./chunk-drv09n16.js';import'./chunk-DCuzAS7H.js';import'./main-HKE7ZUTZ.js';var m=o(t=>`${_1(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),i=m,B=B1({defaultLayout:"swimlane",styles:i});export{B as diagram};
